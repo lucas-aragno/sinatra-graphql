515d2b82e22b4051e16cc79cd414a21c466f8c3a
lucas <lucas.aragno157@gmail.com>
2017-08-03 21:10:02 -0300

    GraphQL endpoint

--- graph/mutations/todo_mutations.rb
+++ graph/mutations/todo_mutations.rb
@@ -1,3 +1,5 @@
+require_relative "../types/todo_type"
+
 module TodoMutations
   UpdateTodo = GraphQL::Relay::Mutation.define do
     name 'UpdateTodo'
@@ -7,7 +9,19 @@ module TodoMutations
 
     return_field :todo, TodoType
     resolve -> (args, ctx) {
-      
+
+    }
+  end
+
+  CreateTodo = GraphQL::Relay::Mutation.define do
+    name 'CreateTodo'
+    description 'Creates a Todo'
+    input_field :title, !types.String
+    input_field :content, types.String
+
+    return_field :todo, TodoType
+    resolve -> (args, ctx) {
+      puts "===== #{args}"
     }
   end
 end
--- routes/graphql.rb
+++ routes/graphql.rb
@@ -1,7 +1,30 @@
+require "json"
+require_relative "../graph/application_schema"
+
 module TodoApp
   module Routes
     class Graphql < Sinatra::Application
       post "/graphql" do
+        query_string = params[:query]
+        query_variables = ensure_hash(params[:variables])
+        result = ApplicationSchema.execute(
+          query_string,
+          variables: query_variables,
+          context: {}
+        )
+        render json: result
+      end
+
+      private
+
+      def ensure_hash(query_variables)
+        if query_variables.blank? || query_variables == "null"
+          {}
+        elsif query_variables.is_a?(String)
+          JSON.parse(query_variables)
+        else
+          query_variables
+        end
       end
     end
   end
